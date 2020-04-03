893707af036b0bc7901263d194208f6da2969e39
Prachi Singh <nero11223344@gmail.com>
2020-04-03 12:14:42 +0530

    set up directory structure and fire up express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port , function(err){
+    if(err){
+        console.log(`error starting the server ${err}`);
+    }
+
+    console.log(`server up and running at port ${port}`);
+})
+
