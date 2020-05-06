e5ee71f70bf952da575176ffbde98838e8b6eb28
Prachi Singh <nero11223344@gmail.com>
2020-05-06 09:47:55 +0530

    move manual authentication to seperate branch

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,9 +1,29 @@
 const User = require('../model/user')
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title:'PROFILE',
-    });
+
+    if(req.cookies.user_id){
+
+        User.findById(req.cookies.user_id , function(err,user){
+
+            if(user){
+                return res.render('user_profile',{
+                    title: 'User Profile',
+                    user : user,
+                    
+                })
+            }
+            else{
+                console.log('user not found in profile');
+                return res.redirect('/users/sign-in');
+            }
+        })
+
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+    
 }
 
 module.exports.posts = function(req,res){
@@ -25,59 +45,9 @@ module.exports.signUp = function(req,res){
 
 //get Sign up data
 module.exports.create = function(req,res){
-
-    if(req.body.password != req.body.confirm_password){
-        console.log('pass does not match')
-        return res.redirect('back');
-    }
-
-    console.log(req.body);
-
-    User.findOne({email : req.body.email}, function(err,user){
-
-        if(err){
-
-            console.log('error in finding user during sign up',err.name); 
-            return;
-       }
-
-        if(!user){
-            User.create(req.body,function(err,user){
-                if(err){console.log('error in creating user during sign up'); return}
-
-                return res.redirect('/users/sign-in');
-            })
-        }
-
-        else{
-            console.log('user already exists');
-            return res.redirect('back');
-        }
-
-    })
-    
-
-
 }
 
 //get Sign in data
 module.exports.createSession = function(req,res){
-    User.findOne(req.body,function(err,user){
-        console.log('error in finding user during sign in');
-
-        //handle user found
-        if(user){
-            //user password does not match
-            if(user.password != req.body.password){
-                return res.redirect('back');
-            }
-
-            // create session
-            res.cookie('user_id',user.id);
-            return res.redirect('/users/profile');
-        }
-        else{
-            //handle user not found
-        }
-    })
+   
 }
