af660fb4b9515e3aad1e50033b864d2082ed59f8
Prachi Singh <nero11223344@gmail.com>
2020-05-08 09:23:24 +0530

    testing git merge

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -45,9 +45,43 @@ module.exports.signUp = function(req,res){
 
 //get Sign up data
 module.exports.create = function(req,res){
+
+    if(req.body.password != req.body.confirm_password){
+        console.log('pass does not match')
+        return res.redirect('back');
+    }
+
+    console.log(req.body);
+
+    User.findOne({email : req.body.email}, function(err,user){
+
+        if(err){
+
+            console.log('error in finding user during sign up',err.name); 
+            return;
+}
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('error in creating user during sign up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }
+
+        else{
+            console.log('user already exists');
+            return res.redirect('back');
+        }
+
+    })
+    
+
+
 }
 
 //get Sign in data
 module.exports.createSession = function(req,res){
-   
+    
 }
+
